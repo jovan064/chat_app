4a3789870628ef8b2978acae71a0598e6ffeb203
Jovan Gajic <jovangajic2000@gmail.com>
2024-05-11 20:48:21 +0200

    added user logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/screens/chat.dart';
+import 'package:chat_app/screens/splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -25,8 +26,11 @@ class App extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
       home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (ctx, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting) {
+          return const SplashScreen();
+        }
         if(snapshot.hasData) {
-          return ChatScreen();
+          return const ChatScreen();
         }
         return const AuthScreen();
       },),
--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter chat'),
+        actions: [
+          IconButton(onPressed: () {
+            FirebaseAuth.instance.signOut();
+          }, icon: const Icon(Icons.exit_to_app, ),),
+        ],
       ),
       body: const Center(child: Text('Logged in'),),
     );
