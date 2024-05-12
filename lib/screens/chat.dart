f764d65952bc8f9d6126b832f9bf4bc24e278902
Jovan Gajic <jovangajic2000@gmail.com>
2024-05-12 17:23:24 +0200

    finishing chat messages

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,8 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -14,7 +17,12 @@ class ChatScreen extends StatelessWidget {
           }, icon: const Icon(Icons.exit_to_app, ),),
         ],
       ),
-      body: const Center(child: Text('Logged in'),),
+      body: const Column(
+        children: [
+        Expanded(child: ChatMessages()),
+        NewMessage(),
+          ],
+      ),
     );
   }
 }
